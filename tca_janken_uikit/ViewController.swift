61c031b41ac1585a099ce4ee279cfb311a9ab58b
shinohara-san <shinohara.yuki.2250@moneyforward.co.jp>
2023-03-25 15:06:07 +0900

    Update: create main screen

---
+++ tca_janken_uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tca_janken_uikit
+//
+//  Created by shinohara.yuki.2250 on 2023/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
