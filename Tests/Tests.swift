3c575e490a4c98b473cd4eeca9fd96b62d4ccc08
shinohara-san <shinohara.yuki.2250@moneyforward.co.jp>
2023-03-25 21:26:28 +0900

    Update: tca style tests

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -9,7 +9,17 @@ import XCTest
 @testable import tca_janken_uikit
 import ComposableArchitecture
 
+@MainActor
 final class Tests: XCTestCase {
+    var store: TestStore<Janken.State, Janken.Action, Janken.State, Janken.Action, ()>? = nil
+
+    override func setUpWithError() throws {
+        store = TestStore(
+            initialState: Janken.State(),
+            reducer: Janken()
+        )
+    }
+
     func testJudge() {
         var result = judge(mine: .gu, computer: .choki)
         XCTAssertEqual(.win, result)
@@ -27,5 +37,44 @@ final class Tests: XCTestCase {
         XCTAssertEqual(UIImage(named:"janken_choki")!, img)
         img = drawComputerHand(.pa)
         XCTAssertEqual(UIImage(named:"janken_pa")!, img)
+    }
+
+    func testJudge() async {
+        await store?.send(.judge(myHand: .gu, comHand: .choki)) {
+            $0.score = 1
+            $0.isShowingAlert = false
+            $0.alertMessage = nil
+        }
+    }
+
+    func testAlert() async {
+        await store?.send(.showAlert(message: "勝ち！")) {
+            $0.isShowingAlert = true
+            $0.alertMessage = "勝ち！"
+        }
+
+        await store?.send(.dismissAlert) {
+            $0.isShowingAlert = false
+            $0.alertMessage = nil
+            $0.image = UIImage(named: "janken_top")
+        }
+    }
+
+    func testReset() async {
+        await store?.send(.showActionSheet) {
+            $0.isShowingActionSheet = true
+        }
+
+        await store?.send(.dismissActionSheet) {
+            $0.isShowingActionSheet = false
+        }
+
+        await store?.send(.showActionSheet) {
+            $0.isShowingActionSheet = true
+        }
+
+        await store?.send(.resetScore) {
+            $0 = Janken.State()
+        }
     }
 }
