4a885ea2606c75df3bf7520424188d8e94d08c3e
shinohara-san <shinohara.yuki.2250@moneyforward.co.jp>
2023-03-25 15:13:01 +0900

    Update: add IBOutlet

--- tca_janken_uikit/ViewController.swift
+++ tca_janken_uikit/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    @IBAction private func didTapGu(_ sender: UIButton) {
     }
 
+    @IBAction private func didTapChoki(_ sender: UIButton) {
+    }
 
+    @IBAction private func didTapPa(_ sender: UIButton) {
+    }
 }
 
